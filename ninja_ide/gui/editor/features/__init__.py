435cd88a2f349dbaea4c1e55a8eab8b127c76c2c
centaurialpha <acostadariogabriel@gmail.com>
2018-12-01 04:49:07 -0300

    Add object to represent a Editor Feature

---
+++ ninja_ide/gui/editor/features/__init__.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of NINJA-IDE (http://ninja-ide.org).
+#
+# NINJA-IDE is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# NINJA-IDE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt5.QtGui import QColor
+
+
+class Feature:
+    """
+    Base class for editor features.
+
+    A `Feature` is something that can be registered in the Editor and add
+    functionality (behavior or visually). All feature can be enabled
+    or disabled through `Feaure.enabled` property.
+    """
+
+    @property
+    def enabled(self) -> bool:
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, value: bool):
+        if value != self._enabled:
+            self._enabled = value
+            self._state_changed(value)
+
+    def __init__(self):
+        self._enabled = False
+        self._editor = None
+        self.color = _Color()
+        self.initialize()
+
+    def initialize(self):
+        pass
+
+    def register(self, editor):
+        self._editor = editor
+        self.enabled = True
+
+    def unregister(self):
+        self._editor = None
+        self.enabled = False
+
+    def _state_changed(self, state: bool):
+        if state:
+            self.on_enabled()
+        else:
+            self.on_disabled()
+
+    def on_enabled(self):
+        """
+        Do what you want when you enable the Feature.
+        Common use would be to connect signals with the Editor.
+        """
+
+    def on_disabled(self):
+        """
+        Do what you want when you disable the Feature.
+        Common use would be to disconnect signals with the Editor.
+        """
+
+
+class _Color:
+    """Class to manage values related to color of a `Feature`"""
+
+    @property
+    def foreground(self):
+        return self._foreground_color
+
+    @foreground.setter
+    def foreground(self, color):
+        if isinstance(color, str):
+            color = QColor(color)
+        self._foreground_color = color
+        # self.update()
+
+    @property
+    def background(self):
+        return self._background_color
+
+    @background.setter
+    def background(self, color):
+        if isinstance(color, str):
+            color = QColor(color)
+        self._background_color = color
+        # self.update()
+
+    @property
+    def alpha(self):
+        return self._alpha
+
+    @alpha.setter
+    def alpha(self, value: int):
+        self._alpha = value
+        # self.update()
+
+    def __init__(self):
+        self._foreground_color = QColor('white')
+        self._background_color = QColor('black')
+        self._alpha = 255
---
+++ ninja_ide/gui/editor/features/current_line.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of NINJA-IDE (http://ninja-ide.org).
+#
+# NINJA-IDE is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+#
+# NINJA-IDE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NINJA-IDE; If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt5.QtGui import QPainter
+
+from ninja_ide.gui.editor.features import Feature
+from ninja_ide.gui.editor.extra_selection import ExtraSelection
+
+_LINE_MODE = 'line'
+_FULL_MODE = 'full'
+
+
+class CurrentLine(Feature):
+
+    @property
+    def mode(self) -> str:
+        return self._mode
+
+    @mode.setter
+    def mode(self, mode: str):
+        self._mode = mode
+        self.update()
+
+    def initialize(self):
+        self._mode = _LINE_MODE
+        self.color.background = 'white'
+        self._color = self.color.background
+
+    def on_enabled(self):
+        self._editor.painted.connect(self._paint)
+
+    def on_disabled(self):
+        self._editor.painted.disconnect(self._paint)
+
+    def _paint(self, event):
+        if self._mode == _FULL_MODE:
+            selection = ExtraSelection(
+                self._editor.textCursor()
+            )
+            selection.set_full_width()
+            selection.set_background(self._color)
+            self._editor.extra_selections['current_line'] = selection
+        else:
+            block = self._editor.textCursor().block()
+            layout = block.layout()
+            line_count = layout.lineCount()
+            line = layout.lineAt(line_count - 1)
+            if line_count < 1:
+                return
+            offset = self._editor.contentOffset()
+            top = self._editor.blockBoundingGeometry(block).translated(
+                offset).top()
+            line_rect = line.naturalTextRect().translated(offset.x(), top)
+            painter = QPainter(self._editor.viewport())
+            painter.setPen(self._color)
+            painter.drawLine(
+                line_rect.x(),
+                line_rect.y() + self._editor.fontMetrics().height() + 5,
+                self._editor.width(),
+                line_rect.y() + self._editor.fontMetrics().height() + 5
+            )
